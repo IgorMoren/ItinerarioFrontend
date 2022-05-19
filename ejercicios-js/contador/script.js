5bc982a60bfecc42238b0692335bce145e0f7c38
IgorMoren <igor.moreno@bosonit.com>
2022-05-19 13:41:17 +0200

    Comentarios añadidos
    Y un poco de optimización... creo

--- ejercicios-js/contador/script.js
+++ ejercicios-js/contador/script.js
@@ -2,18 +2,25 @@ let intervalo;
 
 let numInic;
 
+// Esta variable controla si la funcion start() sumara o restara
 let plusMinus = true;
 
 let rapidez = 1000;
 
+
+
 function start() {
+    //Manejo del estado de los botones capturados con querySelector
     document.querySelector('.btn-start').disabled = true;
     document.querySelector('.btn-reset').disabled = true;
     document.querySelector('.btn-stop').disabled = false;
+    //setInterval ejecutara la funcion timer() cada "rapidez" ms
     intervalo = setInterval(function timer() {
+        //Entrara en una condicion u otra segun
         if (plusMinus) {
+            //Captura del valor de resultado
             let result = getNumero();
-
+            //Captura del valor de los steps indicados en el input type number
             let step = getStep();
 
             result += step;
@@ -28,7 +35,6 @@ function start() {
 
             document.getElementById('resultado').innerHTML = result;
         }
-        console.log(rapidez);
     }, rapidez);
 
 }
@@ -57,9 +63,14 @@ function stop() {
     document.querySelector('.btn-start').disabled = false;
     document.querySelector('.btn-reset').disabled = false;
     document.querySelector('.btn-stop').disabled = true;
+    
+    /* clearInterval() para la ejecucion de la funcion set interval asociada a 
+    la variable intervalo */
     clearInterval(intervalo);
 }
 
+/* reset() establece la variable del campo resultado al valor de numero-base 
+para que el contador lo setee en ese valor */
 function reset() {
     document.getElementById('resultado').innerHTML = getStarter();
 }
--- ejercicios-js/search-on-type/script.js
+++ ejercicios-js/search-on-type/script.js
@@ -1,3 +1,5 @@
+
+
 //Recogemos el campo donde insertaremos lo obtenido del fetch
 const HTMLResponse = document.querySelector('#app');
 
@@ -10,7 +12,7 @@ let timer;                  //Identificador del timer
 let intervalo = 500;        // 0.5 segundo
 let miInput = document.getElementById('resultado');
 
-//on keyup comienza la cuenta atras
+//on keyup comienza la cuenta atras para que ejecute myFunction al 0.5segundos
 miInput.addEventListener('keyup', () => {
     clearTimeout(timer);
     if (miInput.value) {
@@ -20,9 +22,10 @@ miInput.addEventListener('keyup', () => {
 
 /* Trabajemos el cambiar pais */
 
-//INTENTO 2
+//INTENTO 1
 
-
+/*Mediante este intento consiguo modificar el estylo del boton, pero me resulta un codigo muy
+poco optimizado */
 /* selectSpain.addEventListener( 'click', () =>{
     pais = selectSpain.id;
     console.log(selectSpain);
@@ -50,29 +53,36 @@ selectArgentina.addEventListener( 'click', () =>{
 } ) */
 
 
-//INTENTO 1
+//INTENTO 2
 const selectPais = document.getElementsByName('pais');
 
+//Recorre array de paises para que 'escuche' el evento click de cada uno de ellos
+/*¿PREGUNTA? Flujo del siguiente fragmento de código, este codigo, esta ejecutandose 
+todo el rato y captura cuando se realiza el 'click', o recorre el array de paises al 
+hacer click y se queda con el valor capturado? */
 for (let i = 0; i < selectPais.length; i++) {
 
-
     selectPais[i].addEventListener('click', () => {
+        //Obtencion del pais que usaremos en el fetch
         pais = selectPais[i].value;
-        // console.log(selectPais);
+
         let newSelectPais = Array.from(selectPais);
-        //console.log(newSelectPais);
+        
+        //Nos quedamos con el unico pais que nos interesa para manejar el estilo de los botones
         let result = newSelectPais.filter(item => item == newSelectPais[i]);
-        //console.log(selectPais[i]);
-        console.log(result);
+
+        const buttonPais = document.getElementById(pais);
+
+        buttonPais.className = 'buttonstyle';
+
+        /* No consigo optimizarlo mejor, tengo que recorrer de nuevo el array
+        para poder modificar el estilo del resto de botones no clickados */
         for (let f = 0; f < selectPais.length; f++) {
-            console.log(selectPais[f])
-            if (result[0] == selectPais[f]) {
-                /* TODO */
-                console.log('estamos dentro');
+            if (result[0] != selectPais[f]) {
+                const buttonPaisDisable = document.getElementById(selectPais[f].id);
+                buttonPaisDisable.className = '';
             }
         }
-
-
     })
 }
 
@@ -107,24 +117,40 @@ for (let i = 0; i < selectPais.length; i++) {
 // MANERAS FORMATEAR EL HTML:  Creacion del html con string's
 function myFunction() {
 
-    const API_URL = `http://universities.hipolabs.com/search?country=${pais}&name=`;
-
     var input = document.getElementById('resultado').value;
 
-    HTMLResponse.innerHTML = '';
+    //Entra si pais se ha definido, es decir, si ha clickado el pais en el que queremos buscar
+    if (typeof pais !== 'undefined') {
+        const API_URL = `http://universities.hipolabs.com/search?country=${pais}&name=`;
 
-    fetch(`${API_URL}${input}`)
-        .then((response) => {
-            response.json()
-                .then(data => {
-                    for (let i = 0; i < data.length; i++) {
 
-                        const tpl = `<li> ${data[i]['name']} </li>`;
-                        HTMLResponse.innerHTML += `<ul>${tpl}</ul>`;
-                        console.log(data[i]['name']);
+        HTMLResponse.innerHTML = '';
+        //TODO Manejo de errores
+        /*Fetch (promise), la construccion del html tiene que ser dentro del fetch.
+        Si mal no lo entiendo, promises encadenadas, es decir, ¿tenemos respuesta?->
+        json de la respuesta para obtener el formato en el que podemos manejarla. (parse string to json)
+        ¿Formato correcto? Una vez que tenemos los datos en formato correcto se recorre con un for
+        en el que genera un 'template' con los datos y los va añadiendo (+=) en la
+        constante HTMLResponse */
+        fetch(`${API_URL}${input}`)
+            .then((response) => {
+                response.json()
+                    .then(data => {
+                        //Mediante bucle for
+                        /* for (let i = 0; i < data.length; i++) {
+    
+                            const tpl = `<li> ${data[i]['name']} </li>`;
+                            HTMLResponse.innerHTML += `<ul>${tpl}</ul>`;
+                            
+                        } */
+                        //Mediante .map:
+                        //TODO La coma intrusa, no consiguo ver donde esta entrando
+                        const tpl = data.map((universidad) => `<li> ${universidad.name} </li>`);
+
+                        HTMLResponse.innerHTML = `<ul>${tpl}</ul>`;
 
                     }
-                })
-        });
-
+                    )
+            });
+    } else HTMLResponse.innerHTML = `<p> Selecciona el pais de busqueda </p>`;
 }
